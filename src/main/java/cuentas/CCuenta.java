987009252289a643c650b8102e8ad9dffc50d6b2
Carlos <Carlos@DESKTOP-CLP3L8G>
2023-01-20 12:40:05 +0100

    Subimos nuestro segundo commit al repositorio local con comentarios javadoc

--- src/main/java/cuentas/CCuenta.java
+++ src/main/java/cuentas/CCuenta.java
@@ -1,17 +1,53 @@
 package cuentas;
 
-public class CCuenta {
+/**
+ *
+ * Clase que simula una cuenta bancaria
+ * @author Carlos Martínez Matas
+ * @version 1.0
+ */
+public class CCuenta{
 
-
+    /**
+     * Nombre de la Cuenta
+     * @var nombre nombre de la cuenta
+     * @type String
+     */
     private String nombre;
+    
+    /**
+     * 
+     * @var cuenta Iban de la cuenta
+     * @type String
+     */
     private String cuenta;
+    
+    /**
+     * @var saldo Saldo de la cuenta
+     * @type double
+     */
     private double saldo;
+    
+    /**
+     * @var tipoInterés tipo de interés de la cuenta
+     * @type double
+     */
     private double tipoInterés;
 
+    /**
+     * Constructor por defecto
+     */
     public CCuenta()
     {
     }
 
+    /**
+     * Constructor con parámetros
+     * @param nom Nombre del usuario de la cuenta
+     * @param cue Iban de la cuenta
+     * @param sal Saldo de la cuenta
+     * @param tipo tipo de interes
+     */
     public CCuenta(String nom, String cue, double sal, double tipo)
     {
         nombre =nom;
@@ -19,11 +55,20 @@ public class CCuenta {
         saldo=sal;
     }
 
+    /**
+     *
+     * @return retorna el saldo
+     */
     public double estado()
     {
         return getSaldo();
     }
 
+    /**
+     * Ingresa cantidad recibida por parametro al saldo de la cuenta
+     * @param cantidad
+     * @throws Exception
+     */
     public void ingresar(double cantidad) throws Exception
     {
         if (cantidad<0)
@@ -31,6 +76,11 @@ public class CCuenta {
         setSaldo(getSaldo() + cantidad);
     }
 
+    /**
+     * Retira cantidad recibida por parametro del saldo de la cuenta
+     * @param cantidad
+     * @throws Exception
+     */
     public void retirar(double cantidad) throws Exception
     {
         if (cantidad <= 0)
